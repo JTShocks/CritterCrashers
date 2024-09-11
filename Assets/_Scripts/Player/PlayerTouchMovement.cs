5b23cdcd16344e617c278cee45508dbce0cd8fee
JTShocks <jacobdreyer2002@gmail.com>
2024-09-11 16:34:08 -0400

    Added HandleFingerDown

--- Assets/_Scripts/Player/PlayerTouchMovement.cs
+++ Assets/_Scripts/Player/PlayerTouchMovement.cs
@@ -1,10 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
+using ETouch = UnityEngine.InputSystem.EnhancedTouch;
 
 public class PlayerTouchMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    [SerializeField]
+    private Vector2 joystickSize = new Vector2(300,300);
+    [SerializeField]
+    private FloatingJoystick Joystick;
+    [SerializeField]
+    private PlayerController Player;
+
+    private Finger movementFinger;
+    private Vector2 movementAmount; 
+
+    void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+        ETouch.Touch.onFingerDown += HandleFingerDown;
+        ETouch.Touch.onFingerUp += HandleFingerUp;
+        ETouch.Touch.onFingerMove += HandleFingerMove;
+    }
+
+    void OnDisable()
+    {
+        ETouch.Touch.onFingerDown -= HandleFingerDown;
+        ETouch.Touch.onFingerUp -= HandleFingerUp;
+        ETouch.Touch.onFingerMove += HandleFingerMove;
+        EnhancedTouchSupport.Disable();
+    }
+    
+
+
     void Start()
     {
         
@@ -14,5 +43,45 @@ public class PlayerTouchMovement : MonoBehaviour
     void Update()
     {
         
+    }
+    private void HandleFingerMove(Finger touchedFinger)
+    {
+        
+    }
+    private void HandleFingerUp(Finger touchedFinger)
+    {
+        
+    }
+
+    private void HandleFingerDown(Finger touchedFinger)
+    {
+        if(movementFinger == null && touchedFinger.screenPosition.x <= Screen.width /2f)
+        {
+            movementFinger = touchedFinger;
+            movementAmount = Vector2.zero;
+            Joystick.gameObject.SetActive(true);
+            Joystick.RectTransform.sizeDelta = joystickSize;
+            Joystick.RectTransform.anchoredPosition = ClampStartPosition(touchedFinger.screenPosition);
+
+        }
+    }
+
+    private Vector2 ClampStartPosition(Vector2 startPosition)
+    {
+        if(startPosition.x < joystickSize.x /2)
+        {
+            startPosition.x = joystickSize.x / 2;
+        }
+
+        if(startPosition.y < joystickSize.y / 2)
+        {
+            startPosition.y = joystickSize.y / 2;
+        }
+        else if(startPosition.y > Screen.height - joystickSize.y/2)
+        {
+            startPosition.y = Screen.height - joystickSize.y / 2;
+        }
+
+        return startPosition;
     }
 }

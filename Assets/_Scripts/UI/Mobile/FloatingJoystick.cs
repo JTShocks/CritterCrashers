c706138a2c5c7b709fc6598bdabc6f5ccb56d5e7
JTShocks <jacobdreyer2002@gmail.com>
2024-09-11 16:15:42 -0400

    Added FloatingJoystick script

--- Assets/_Scripts/UI/Mobile/FloatingJoystick.cs
+++ Assets/_Scripts/UI/Mobile/FloatingJoystick.cs
@@ -1,18 +1,15 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(RectTransform))]
+[DisallowMultipleComponent]
 public class FloatingJoystick : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [HideInInspector]
+    public RectTransform RectTransform;
+    public RectTransform Knob;
 
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
-        
+        RectTransform = GetComponent<RectTransform>();
     }
 }

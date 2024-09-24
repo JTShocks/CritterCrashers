ed8db6e4879f71309ba47360b862d01cdeb6644a
JTShocks <jacobdreyer2002@gmail.com>
2024-09-24 13:19:56 -0400

    Player can move with WASD

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -10,16 +10,28 @@ public class PlayerController : MonoBehaviour
     public CarController car;
 
 
-   // #if UNITY_ANDROID
+    
+    #if UNITY_ANDROID
     private PlayerTouchMovement playerTouchMovement;
-    //#endif
+    #endif
+
+    void OnEnable()
+    {
+        PlayerInput.onMove += MovementInput;
+        
+    }
+
+    void OnDisable()
+    {
+        PlayerInput.onMove -= MovementInput;
+    }
 
     // Start is called before the first frame update
     void Awake()
     {
-       // #if UNITY_ANDROID
+        #if UNITY_ANDROID
             playerTouchMovement = GetComponent<PlayerTouchMovement>();
-        //#endif
+        #endif
 
     }
 
@@ -27,21 +39,26 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
 
-        //#if UNITY_ANDROID
+        #if UNITY_ANDROID
         //Only in the Android build, should the player input component be read
+           // MovementInput(playerTouchMovement.movementAmount);
+        #endif
 
-        inputVector = new Vector3(playerTouchMovement.movementAmount.x, 0, playerTouchMovement.movementAmount.y);
-        inputVector.Normalize();
-                car.Steer(inputVector);
 
-        //#endif
         
+        car.Steer(inputVector);
+
+        
+    }
+
+    void MovementInput(Vector2 input)
+    {
+        inputVector = new Vector3(input.x, 0, input.y);
+        inputVector.Normalize();
     }
 
     void FixedUpdate()
     {
         car.Move(inputVector);
-
-
     }
 }

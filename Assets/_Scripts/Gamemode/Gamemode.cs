01b06e744c6f4a77fce632d68107614a90311cf5
JTShocks <jacobdreyer2002@gmail.com>
2024-10-07 16:12:17 -0400

    Initial commit - Added Gamemode script

---
+++ Assets/_Scripts/Gamemode/Gamemode.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamemode : MonoBehaviour
+{
+    //Gamemode is a base class for all other gamemodes
+    //When a gamemode is loaded, it is initialized with all the requirements required of it
+    public virtual void Initialize()
+    {
+
+        
+    }
+
+
+}

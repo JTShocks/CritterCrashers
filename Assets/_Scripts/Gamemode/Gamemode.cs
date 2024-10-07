4c7be1173df2d939b14e051109cacb66a40f25d8
JTShocks <jacobdreyer2002@gmail.com>
2024-10-07 16:18:00 -0400

    Update Gamemode.cs
    Added more notes

--- Assets/_Scripts/Gamemode/Gamemode.cs
+++ Assets/_Scripts/Gamemode/Gamemode.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class Gamemode : MonoBehaviour
 {
+    
+    //Initialize the gamemode on the current track, grabbing all the data for the track and the checkpoints
+
+    //For example: A Time attack gamemode connects to the Checkpoint crossed event, by adding time
+    //The gamemode also determines if the game should spawn in other AI racers
+
     //Gamemode is a base class for all other gamemodes
     //When a gamemode is loaded, it is initialized with all the requirements required of it
     public virtual void Initialize()
     {
-
-        
+        Debug.Log("Gamemode is initializing...");
     }
+
+
 
 
 }
